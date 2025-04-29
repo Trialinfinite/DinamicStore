9dedc0479ed875d3a4fabd132aae2ee486bc8235
Kagiso-Kevin <kmotsholabatho@gmail.com>
2025-04-29 17:18:54 +0200

    Update SortUtil.java

--- src/dinamicstore/utils/SortUtil.java
+++ src/dinamicstore/utils/SortUtil.java
@@ -1,1 +1,34 @@
+import java.util.List;
 
+public class SortUtil {
+
+    // Selection Sort by Price
+    public static void selectionSort(List<Product> products) {
+        for (int i = 0; i < products.size() - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < products.size(); j++) {
+                if (products.get(j).getPrice() < products.get(minIdx).getPrice()) {
+                    minIdx = j;
+                }
+            }
+            // Swap
+            Product temp = products.get(i);
+            products.set(i, products.get(minIdx));
+            products.set(minIdx, temp);
+        }
+    }
+
+    // Insertion Sort by Price
+    public static void insertionSort(List<Product> products) {
+        for (int i = 1; i < products.size(); i++) {
+            Product key = products.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && products.get(j).getPrice() > key.getPrice()) {
+                products.set(j + 1, products.get(j));
+                j--;
+            }
+            products.set(j + 1, key);
+        }
+    }
+}
